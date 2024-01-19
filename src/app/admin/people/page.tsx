0a2e7da63872d58b0ab39e1c6f468cce6135dbc9
fghpdf <fghpdf@outlook.com>
2024-01-19 21:49:47 +0900

    chore: clean import

--- src/app/admin/people/page.tsx
+++ src/app/admin/people/page.tsx
@@ -1,37 +1,13 @@
 'use client';
 /*!
-  _   _  ___  ____  ___ ________  _   _   _   _ ___   
- | | | |/ _ \|  _ \|_ _|__  / _ \| \ | | | | | |_ _| 
- | |_| | | | | |_) || |  / / | | |  \| | | | | || | 
- |  _  | |_| |  _ < | | / /| |_| | |\  | | |_| || |
- |_| |_|\___/|_| \_\___/____\___/|_| \_|  \___/|___|
-                                                                                                                                                                                                                                                                                                                                       
-=========================================================
-* Horizon UI - v1.1.0
-=========================================================
-
-* Product Page: https://www.horizon-ui.com/
-* Copyright 2022 Horizon UI (https://www.horizon-ui.com/)
-
-* Designed and Coded by Simmmple
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
 
 // Chakra imports
-import {Box, Flex, Grid, GridItem, Spacer} from '@chakra-ui/react';
-import AdminLayout from 'layouts/admin';
+import {Box, Grid, GridItem} from '@chakra-ui/react';
 
 // Custom components
 import Banner from 'views/admin/profile/components/Banner';
-import General from 'views/admin/profile/components/General';
-import Notifications from 'views/admin/profile/components/Notifications';
-import Projects from 'views/admin/profile/components/Projects';
-import Storage from 'views/admin/profile/components/Storage';
-import Upload from 'views/admin/profile/components/Upload';
 
 // Assets
 import banner from 'img/auth/banner.png';
