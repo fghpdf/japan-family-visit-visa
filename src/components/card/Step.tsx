8594fe83eb237405b7ea48030addb4e25ffdb0ed
fghpdf <fghpdf@outlook.com>
2024-03-03 03:30:21 +0900

    feat: application people step 2

--- src/components/card/Step.tsx
+++ src/components/card/Step.tsx
@@ -85,7 +85,7 @@ const StepForm = () => {
                   <Select placeholder='选择与护照一致的出生地' {...register("birthPlace")}>
                     {
                       provinces.map((province) => (
-                        <option value={province.name}>{province.name}</option>
+                        <option key={province.code} value={province.name}>{province.name}</option>
                       ))
                     }
                   </Select>
@@ -114,17 +114,63 @@ const StepForm = () => {
             </>
           )}
           {step === 2 && (
-            <FormControl>
-              <FormLabel>Step 2 Input</FormLabel>
-              <Input
-                type="text"
-                placeholder="Step 2 Data"
-                value={formData.step2Data}
-                onChange={(e) =>
-                  setFormData({ ...formData, step2Data: e.target.value })
-                }
-              />
-            </FormControl>
+            <>
+              <HStack>
+                <FormControl isRequired width="100%">
+                    <FormLabel>婚姻状况</FormLabel>
+                    <RadioGroup>
+                      <Stack direction='row'>
+                        <Radio value="Single">未婚</Radio>
+                        <Radio value="Married">已婚</Radio>
+                        <Radio value="Widowed">离婚</Radio>
+                        <Radio value="Divorced">丧偶</Radio>
+                      </Stack>
+                    </RadioGroup>
+                </FormControl>
+              </HStack>
+              <Stack>
+                <FormControl isRequired width="100%">
+                    <FormLabel>家庭现住址</FormLabel>
+                    <Input {...register("home_address")} isRequired placeholder="当前居住的地址，比如湖北省武汉市洪山区XX小区"/>
+                </FormControl>
+              </Stack>
+              <Stack>
+                <FormControl isRequired width="100%">
+                    <FormLabel>联系方式</FormLabel>
+                    <Input type="tel" {...register("tel")} isRequired placeholder="13412345678"/>
+                </FormControl>
+              </Stack>
+              <Stack>
+                <FormControl isRequired width="100%">
+                    <FormLabel>电子邮箱</FormLabel>
+                    <Input type="email" {...register("email")}  isRequired placeholder="XX@outlook.com"/>
+                </FormControl>
+              </Stack>
+              <Stack>
+                <FormControl isRequired width="100%">
+                    <FormLabel>任职单位名称</FormLabel>
+                    <Input {...register("employer_name")}  isRequired placeholder="当前公司名，比如武汉初心有限公司"/>
+                </FormControl>
+              </Stack>
+              <Stack>
+                <FormControl isRequired width="100%">
+                    <FormLabel>任职单位联系方式</FormLabel>
+                    <Input type="tel" {...register("employer_tel")}  isRequired placeholder="13412345678"/>
+                </FormControl>
+              </Stack>
+              <Stack>
+                <FormControl isRequired width="100%">
+                    <FormLabel>任职单位地址</FormLabel>
+                    <Input {...register("employer_address")} isRequired placeholder="当前公司的地址，比如湖北省武汉市洪山区光谷软件园"/>
+                </FormControl>
+              </Stack>
+              <Stack>
+                <FormControl isRequired width="100%">
+                    <FormLabel>任职单位职位</FormLabel>
+                    <Input {...register("occupation")} isRequired placeholder="当前职位，比如财务"/>
+                </FormControl>
+              </Stack>
+            </>
           )}
           {/* 根据 `step` 的值来渲染不同的表单输入 */}
           {/* ...更多步骤 */}
