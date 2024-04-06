a9bf3ae313762409eacf0ca07df10953296ed54b
fghpdf <fghpdf@outlook.com>
2024-04-06 23:52:17 +0900

    ✨ feat: add step3 passport informaion and step4 guarantor information

--- src/components/card/Step.tsx
+++ src/components/card/Step.tsx
@@ -131,7 +131,7 @@ const StepForm = () => {
               <Stack>
                 <FormControl isRequired width="100%">
                     <FormLabel>家庭现住址</FormLabel>
-                    <Input {...register("home_address")} isRequired placeholder="当前居住的地址，比如湖北省武汉市洪山区XX小区"/>
+                    <Input {...register("homeAddress")} isRequired placeholder="当前居住的地址，比如湖北省武汉市洪山区XX小区"/>
                 </FormControl>
               </Stack>
               <Stack>
@@ -149,19 +149,19 @@ const StepForm = () => {
               <Stack>
                 <FormControl isRequired width="100%">
                     <FormLabel>任职单位名称</FormLabel>
-                    <Input {...register("employer_name")}  isRequired placeholder="当前公司名，比如武汉初心有限公司"/>
+                    <Input {...register("employerName")}  isRequired placeholder="当前公司名，比如武汉初心有限公司"/>
                 </FormControl>
               </Stack>
               <Stack>
                 <FormControl isRequired width="100%">
                     <FormLabel>任职单位联系方式</FormLabel>
-                    <Input type="tel" {...register("employer_tel")}  isRequired placeholder="13412345678"/>
+                    <Input type="tel" {...register("employerTel")}  isRequired placeholder="13412345678"/>
                 </FormControl>
               </Stack>
               <Stack>
                 <FormControl isRequired width="100%">
                     <FormLabel>任职单位地址</FormLabel>
-                    <Input {...register("employer_address")} isRequired placeholder="当前公司的地址，比如湖北省武汉市洪山区光谷软件园"/>
+                    <Input {...register("employerAddress")} isRequired placeholder="当前公司的地址，比如湖北省武汉市洪山区光谷软件园"/>
                 </FormControl>
               </Stack>
               <Stack>
@@ -172,11 +172,89 @@ const StepForm = () => {
               </Stack>
             </>
           )}
+          {step === 3 && (
+            <>
+              <FormControl isRequired>
+                <FormLabel>护照号</FormLabel>
+                <Input {...register("passportNumber")} isRequired placeholder="护照号"/>
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>护照签发地点</FormLabel>
+                <Input {...register("passportIssuedPlace")} isRequired placeholder="护照签发地点"/>
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>护照签发机关</FormLabel>
+                <Input {...register("passportIssuedAuthority")} isRequired placeholder="护照签发机关"/>
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>护照签发日期</FormLabel>
+                <Input type="date" {...register("passportIssuedDate")} isRequired placeholder="与护照一致"/>
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>护照有效日期</FormLabel>
+                <Input type="date" {...register("passportExpiryDate")} isRequired placeholder="与护照一致"/>
+              </FormControl>
+            </>
+          )}
+          {step === 4 && (
+            <>
+              <FormControl isRequired>
+                <FormLabel>担保人名字（英语或者拼音）</FormLabel>
+                <Input {...register("guarantorNameEn")} isRequired placeholder="与护照一致"/>
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>担保人名字（中文或者日语或者英语）</FormLabel>
+                <Input {...register("guarantorName")} isRequired placeholder="与护照一致"/>
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>担保人联系方式</FormLabel>
+                <Input type="tel" {...register("guarantorTel")} isRequired placeholder="+8109012341234"/>
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>担保人地址（英语）</FormLabel>
+                <Input {...register("guarantorAddressEn")} isRequired placeholder="当前居住的地址，比如 XXXX, Nippori Tokyo"/>
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>担保人地址</FormLabel>
+                <Input {...register("guarantorAddress")} isRequired placeholder="当前居住的地址，比如東京都荒川区日暮里"/>
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>担保人出生年月日</FormLabel>
+                <Input type="date" {...register("guarantorBirthday")} isRequired placeholder="与护照一致"/>
+              </FormControl>
+              <FormControl as="fieldset" isRequired>
+                <FormLabel as="legend">担保人性别</FormLabel>
+                <RadioGroup>
+                  <HStack>
+                    <Radio value="Male">男</Radio>
+                    <Radio value="Female">女</Radio>
+                  </HStack>
+                </RadioGroup>
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>担保人和申请人的关系</FormLabel>
+                <Input {...register("guarantorRelationship")} isRequired placeholder="比如，父子，兄妹，朋友"/>
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>担保人的职位</FormLabel>
+                <Input {...register("guarantorOccupation")} isRequired placeholder="比如，软件工程师"/>
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>担保人的国籍</FormLabel>
+                <Input {...register("guarantorNationality")} isRequired placeholder="比如，中国"/>
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>担保人的签证类型</FormLabel>
+                <Input {...register("guarantorVisa")} isRequired placeholder="比如，高度专门职 1 号"/>
+              </FormControl>
+            </>
+          )}
+
           {/* 根据 `step` 的值来渲染不同的表单输入 */}
           {/* ...更多步骤 */}
           <HStack>
             {step > 1 && <Button onClick={prevStep}>Previous</Button>}
-            {step < 3 ? (
+            {step < 5 ? (
               <Button colorScheme="teal" onClick={nextStep}>
                 Next
               </Button>
